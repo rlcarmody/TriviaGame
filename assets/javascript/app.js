5e9604261da1763ca3f8c94109a98f48f7e3f37f
RLC <rlcarmody@gmail.com>
2019-02-17 19:20:26 -0800

    Commenting and optimizing

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,32 +1,37 @@
 $(document).ready(function () {
+
     var questions = [];
     var answers = [];
     var qNum = 0;
     var correctAnswers = 0;
     var incorrectAnswers = 0;
     var intervalId;
-    var timeLeft = 30;
-    const content = document.getElementById('container');
     const categoriesURL = 'https://opentdb.com/api_category.php';
+    //Queries The Open Trivia DB API
     const getData = async (url) => {
         const json = await fetch(url)
             .then(response => response.json());
         return json;
     }
+    //Builds the list of available categories by calling the API.  Each category has an id property.
     getData(categoriesURL).then(response => {
         let cat = response.trivia_categories;
         cat.forEach(element => {
-            content.innerHTML += `<button class="categories" id="${element.id}">${element.name}</button>`;
+            $('#container').append(`<button class="categories" id="${element.id}">${element.name}</button>`);
         });
     });
+    //Listener for category selection calls API to fetch Q:A pairs and calls function to display first question
     $('body').on('click', '.categories', function () {
-        getData(`https://opentdb.com/api.php?amount=10&category=${this.id}&type=multiple`).then(response => {
-            questions = response.results;
-            showQ(qNum);
-        })
+        getData(`https://opentdb.com/api.php?amount=10&category=${this.id}&type=multiple`)
+            .then(response => {
+                questions = response.results;
+                showQ(qNum);
+            })
     })
+    //Sets up 30 second question timer and displays the question and list of answers. 
+    //The answers are constructed into an array from two properties on the response and sent to a randomizer function
     const showQ = (num) => {
-        timeLeft = 30;
+        let timeLeft = 30;
         $('#timer').text(timeLeft).attr('value', Math.max(timeLeft, 10));
         intervalId = setInterval(function () {
             timeLeft--;
@@ -46,18 +51,13 @@ $(document).ready(function () {
         for (let index in answers) {
             $('#answers').append(`<li class="answer">${answers[index]}</li>`)
         }
-        console.log(answers);
-     
-    }
-    $('body').on('click', '.answer', function () {
-        console.log($(this).html())
-        if ($(this).text() === decodeHTML(questions[qNum].correct_answer)) {
-            showAnswer(true);
-        } else {
-            showAnswer(false);
-        }
 
+    }
+    //Listener for the selected answer and calls the showAnswer function based on whether the user was right or wrong
+    $('body').on('click', '.answer', function () {
+        $(this).text() === decodeHTML(questions[qNum].correct_answer) ? showAnswer(true) : showAnswer(false);
     })
+    //Displays the response based on whether user was correct or not.  Automatically advances to next question after 5 seconds.
     const showAnswer = (status) => {
         clearInterval(intervalId);
         switch (status) {
@@ -76,14 +76,10 @@ $(document).ready(function () {
         }
         qNum++
         setTimeout(function () {
-            if (qNum < 10) {
-                showQ(qNum);
-            }
-            else {
-                $('#container').html(`<h2>Game Over</h2><p>Correct Answers: ${correctAnswers}</p><p>Incorrect Answers: ${incorrectAnswers}</p>`);
-            }
+            qNum < 10 ? showQ(qNum) : $('#container').html(`<h2>Game Over</h2><p>Correct Answers: ${correctAnswers}</p><p>Incorrect Answers: ${incorrectAnswers}</p>`);
         }, 5000)
     }
+    //Shuffles the "Correct Answer" with the array of incorrect answers
     const randomize = (arr) => {
         let currentIndex = arr.length, temporaryValue, randomIndex;
         while (0 !== currentIndex) {
@@ -95,8 +91,10 @@ $(document).ready(function () {
         }
         return arr;
     }
+    //Some answers have escaped HTML characters (such as &amp;). This function allows for straight
+    //comparison of the answers against the user selection.
     const decodeHTML = (str) => {
-        var txt = document.createElement('div');
+        let txt = document.createElement('div');
         txt.innerHTML = str;
         return txt.textContent;
     }
